0ffbda525c95b9d28bd19c6666a42c37f38b4fa1
My Lord <page.c.tyler@gmail.com>
2020-04-18 17:51:32 -0400

    resoved issue with footer not sticking with the home component or the other components and not home when toggling the display css b/t absolute and static.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,8 @@ export default function Footer() {
     const { pathname } = location();
     return (
         <div>
-            <footer className={pathname === '/about' ? 'page-footer font-small m-0' : 'page-footer font-small'} >
+            <footer style={pathname === '/' ? {position:'absolute'} : {position:'static'}}
+                    className={pathname === '/about' ? 'page-footer font-small m-0' : 'page-footer font-small'}>
                 <div className={'footer-copyright text-white text-center py-3'}> 
                     &copy; Copyright 2020
                 </div>
