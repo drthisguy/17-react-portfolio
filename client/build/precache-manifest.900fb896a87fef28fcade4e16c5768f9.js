1110d46e857a546c99381673e7ac58a0ace13df9
My Lord <page.c.tyler@gmail.com>
2020-04-19 01:45:18 -0400

    moved over to MERN template for emailing from form.  finishings.

---
+++ client/build/precache-manifest.900fb896a87fef28fcade4e16c5768f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca2cc3477fed8410ad84b402d875cf0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffd3e21b04e068c636d8",
+    "url": "/static/css/main.b0e8a118.chunk.css"
+  },
+  {
+    "revision": "7c2165a20e2015aafbe3",
+    "url": "/static/js/2.ed248bc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ed248bc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffd3e21b04e068c636d8",
+    "url": "/static/js/main.3e95ef45.chunk.js"
+  },
+  {
+    "revision": "a0ac1140309c735bc60b",
+    "url": "/static/js/runtime-main.6be4d7d3.js"
+  },
+  {
+    "revision": "967724564e39c011ce2e5c6f335b5f7d",
+    "url": "/static/media/greyscaled.96772456.jpg"
+  }
+]);
