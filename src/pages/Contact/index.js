f006650dacb0b54c019316d949170f3a10decc77
My Lord <page.c.tyler@gmail.com>
2020-04-18 21:45:45 -0400

    form component created to handle form submision.

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Container, Row, Col } from '../../components/Grid';
 import LinkList from "../../components/LinkList";
+import { Input, TextArea } from '../../components/Form'
 import { boot, custom } from './style'
 
 export default function Contact() {
 
+    const [email, setEmail] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
 
+    const onInputChange = (e) => {
+        const { name, value } = e.target
+        setEmail({ ...setEmail, [name]: value })
+        console.log(email.name)
+    }
 
     return (
         <div className={custom.flex}>
@@ -32,16 +43,19 @@ export default function Contact() {
                             <form className={boot.body}>
                                 <h2 className={boot.header}>Contact</h2>
                                 <div className={boot.group}>
-                                    <label>Name</label>
-                                    <input className={boot.ctrl} type="text" placeholder="Your Name"/>
+                                    <label>Name:</label>
+                                    <Input onChange={onInputChange} 
+                                    name="name" type="text" placeholder="Your Name"/>
                                 </div>
                                 <div className={boot.group}>
-                                    <label>email</label>
-                                    <input className={boot.ctrl} type="email" placeholder="youremail@example.com"/>
+                                    <label>Email:</label>
+                                    <Input onChange={onInputChange} 
+                                    name="email" type="email" placeholder="youremail@example.com"/>
                                 </div>
                                 <div className={boot.group}>
-                                    <label>Name</label>
-                                    <textarea className={boot.ctrl} rows="6" placeholder="Type your message here..."/>
+                                    <label>Message:</label>
+                                    <TextArea onChange={onInputChange} 
+                                    name="message" rows="6" placeholder="Type your message here..."/>
                                 </div>
 
                                 <Col size={'md-12'} classes={boot.btn} >
