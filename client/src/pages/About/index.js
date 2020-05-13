629bb66c77c78db4d71ff38623721609ce7a524c
My Lord <page.c.tyler@gmail.com>
2020-05-12 22:41:20 -0400

    typo on about page.

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -8,7 +8,7 @@ export default function index() {
             <div className={"card-body"}>
                 <h3 className={"card-title"}>About Me</h3>
                 <hr/>
-                <p className={"card-text"}>With an academic background in physics from Drexel University and web development from the University of Pennsylvania LPS, I have gained an knowledge base & sharpened skills that are geared toward engineering, software development and information technologies. I tend to excel whenever tasked with solving problems with computer-based solutions. Throughout much of my coursework and vocational experiences, I've always worked well in collaboration with others. I showcase good written and oral communications and can grasp many different types of complex systems or subjects. Above all, I have learned to become a trustworthy and hardworking individual who is also a fast, yet careful learner.</p>
+                <p className={"card-text"}>With an academic background in physics from Drexel University and web development from the University of Pennsylvania LPS, I have gained a knowledge base & sharpened skills that are geared toward engineering, software development and information technologies. I tend to excel whenever tasked with solving problems with computer-based solutions. Throughout much of my coursework and vocational experiences, I've always worked well in collaboration with others. I showcase good written and oral communications and can grasp many different types of complex systems or subjects. Above all, I have learned to become a trustworthy and hardworking individual who is also a fast, yet careful learner.</p>
             </div>
         </div>   
      </div>
