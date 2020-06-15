7e8a56fcf0504ee794b99e4387245a96788f88bf
drthisguy <page.c.tyler@gmail.com>
2020-06-15 15:44:04 -0400

    updated about page with new brand statement.

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -8,7 +8,7 @@ export default function index() {
             <div className={"card-body"}>
                 <h3 className={"card-title"}>About Me</h3>
                 <hr/>
-                <p className={"card-text"}>With an academic background in physics from Drexel University and web development from the University of Pennsylvania LPS, I have gained a knowledge base & sharpened skills that are geared toward engineering, software development and information technologies. I tend to excel whenever tasked with solving problems with computer-based solutions. Throughout much of my coursework and vocational experiences, I've always worked well in collaboration with others. I showcase good written and oral communications and can grasp many different types of complex systems or subjects. Above all, I have learned to become a trustworthy and hardworking individual who is also a fast, yet careful learner.</p>
+                <p className={"card-text"}>With a love for coding and learning, along with an academic background in physics from Drexel University and web development from the University of Pennsylvania LPS, I have gained a knowledge base and skillset that are geared toward engineering & software development. Computer based problem solving has become my forte. Throughout much of my coursework and vocational experiences, I've always worked well in collaboration with others, showcased good written and oral communications and can grasped many different kinds of complex systems or subject matters. Above all, I have learned to become a trustworthy and hardworking individual who is also a fast, eager and yet careful learner.  I look forward to and strive to make computing, along with myself, better for the world.</p>
             </div>
         </div>   
      </div>
