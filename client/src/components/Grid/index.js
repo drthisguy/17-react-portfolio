b57e638d5bdad5cecaca618d11f606b0347b52fd
drthisguy <page.c.tyler@gmail.com>
2020-07-16 16:22:38 -0400

    resolved issue where some divs were getting a class of 'undifined' added to its classes. a little cleaner but no real effect on anything.

--- client/src/components/Grid/index.js
+++ client/src/components/Grid/index.js
@@ -1,17 +1,18 @@
 import React from 'react'
 
         
-export const Container = ({ classes, children }) => <div className={`container ${classes}`}>{children}</div>
+export const Container = ({ classes, children }) => <div className={classes ? `container ${classes}`: 'container'}>{children}</div>
 
 export const Collapse = ({ classes, children, id }) => <div className={classes} id={id}>{children}</div>
 
-export const Row = ({ classes, children }) => <div className={`row ${classes}`}>{children}</div>
+export const Row = ({ classes, children }) => <div className={classes ? `row ${classes}`: 'row'}>{children}</div>
 
 export const Col = ({ size, classes, children }) => {
-return ( <div 
-    className={size.split(' ').map( size => `col-${size}`).join(' ')
-    +" "+ classes}>{children}</div>
- )
-}           
-         
+    const theClass = classes ? classes : ''
+    return ( <div 
+        className={size.split(' ').map( size => `col-${size}`).join(' ')
+        +" "+ theClass}>{children}
+        </div>
+    )
+}       
     
