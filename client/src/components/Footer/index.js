b31d85f225f7808c6d5d1aaeda7edcb80eb3cabe
drthisguy <page.c.tyler@gmail.com>
2020-09-08 14:50:00 -0400

    resolved issue where footer would relocate when displayed on screens smaller than 768px.

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -7,7 +7,7 @@ export default function Footer({ navState }) {
     return (
         <div>
             <footer 
-                    style={!navState && (pathname === '/' || pathname === '/contact') ? {position:'absolute'} : {position:'static'}}
+                    style={!navState && (pathname === '/' || pathname === '/contact') && window.innerWidth > 768 ? {position:'absolute'} : {position:'static'}}
                     className={(pathname === '/about') ? 'page-footer font-small foot-pos' : 'page-footer font-small'}>
                 <div className={'footer-copyright text-white text-center py-3'}> 
                     &copy; Copyright 2020
